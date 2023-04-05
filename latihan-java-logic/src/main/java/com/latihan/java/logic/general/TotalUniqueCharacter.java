b580761b4cc934e355b047e13b02ad6741e94584
Your <aant.sultan@gmail.com>
2023-04-05 08:25:02 +0700

    adding new logic for total unique character without collection class

--- latihan-java-logic/src/main/java/com/latihan/java/logic/general/TotalUniqueCharacter.java
+++ latihan-java-logic/src/main/java/com/latihan/java/logic/general/TotalUniqueCharacter.java
@@ -6,7 +6,7 @@ public class TotalUniqueCharacter {
 
     public static void main(String[] args) {
         String input = "aaAAaazzzzZz";
-        System.out.println(totalCharacter(input));
+        System.out.println(totalCharacter2(input));
     }
 
     static int totalCharacter(String input) {
@@ -14,5 +14,18 @@ public class TotalUniqueCharacter {
                 .chars()
                 .mapToObj(c -> c)
                 .collect(Collectors.toSet()).size();
+    }
+
+    static int totalCharacter2(String input){
+        char[] inputArr = input.toLowerCase().toCharArray();
+        int total = 0;
+        String newInput = "";
+        for(int i = 0; i < inputArr.length; i++){
+            if(!newInput.contains(String.valueOf(inputArr[i]))){
+                newInput += String.valueOf(inputArr[i]);
+                total ++;
+            }
+        }
+        return total;
     }
 }
