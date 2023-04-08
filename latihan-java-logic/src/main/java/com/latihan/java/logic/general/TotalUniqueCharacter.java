4e933c3903d6ead5774d38a12ed17e28d8de3643
Your <aant.sultan@gmail.com>
2023-04-08 13:03:27 +0700

    adding new function with recursive method

--- latihan-java-logic/src/main/java/com/latihan/java/logic/general/TotalUniqueCharacter.java
+++ latihan-java-logic/src/main/java/com/latihan/java/logic/general/TotalUniqueCharacter.java
@@ -8,7 +8,7 @@ public class TotalUniqueCharacter {
 
     public static void main(String[] args) {
         String input = "aaAAaazzzzZz";
-        System.out.println(totalCharacter3(input));
+        System.out.println(totalCharacter4(input));
     }
 
     static int totalCharacter(String input) {
@@ -18,29 +18,38 @@ public class TotalUniqueCharacter {
                 .collect(Collectors.toSet()).size();
     }
 
-    static int totalCharacter2(String input){
+    static int totalCharacter2(String input) {
         char[] inputArr = input.toLowerCase().toCharArray();
         int total = 0;
         String newInput = "";
-        for(int i = 0; i < inputArr.length; i++){
-            if(!newInput.contains(String.valueOf(inputArr[i]))){
+        for (int i = 0; i < inputArr.length; i++) {
+            if (!newInput.contains(String.valueOf(inputArr[i]))) {
                 newInput += String.valueOf(inputArr[i]);
-                total ++;
+                total++;
             }
         }
         return total;
     }
 
-    static int totalCharacter3(String input){
+    static int totalCharacter3(String input) {
         char[] inputArr = input.toLowerCase().toCharArray();
         int total = 0;
         List<Character> list = new ArrayList<>();
-        for(int i = 0; i < inputArr.length; i++){
-            if(!list.contains(inputArr[i])){
+        for (int i = 0; i < inputArr.length; i++) {
+            if (!list.contains(inputArr[i])) {
                 list.add(inputArr[i]);
-                total ++;
+                total++;
             }
         }
         return total;
+    }
+
+    static int totalCharacter4(String input) {
+        if (input.length() == 0) {
+            return 0;
+        } else {
+            String lower = input.toLowerCase();
+            return 1 + totalCharacter4(lower.replace(String.valueOf(lower.charAt(0)), ""));
+        }
     }
 }
