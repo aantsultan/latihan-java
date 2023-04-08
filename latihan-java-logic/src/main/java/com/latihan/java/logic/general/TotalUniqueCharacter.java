f3cffeee537d520d314ff4e3e37deaf81589ba7d
Your <aant.sultan@gmail.com>
2023-04-08 12:51:07 +0700

    adding new function for get total unique character

--- latihan-java-logic/src/main/java/com/latihan/java/logic/general/TotalUniqueCharacter.java
+++ latihan-java-logic/src/main/java/com/latihan/java/logic/general/TotalUniqueCharacter.java
@@ -1,12 +1,14 @@
 package com.latihan.java.logic.general;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class TotalUniqueCharacter {
 
     public static void main(String[] args) {
         String input = "aaAAaazzzzZz";
-        System.out.println(totalCharacter2(input));
+        System.out.println(totalCharacter3(input));
     }
 
     static int totalCharacter(String input) {
@@ -23,6 +25,19 @@ public class TotalUniqueCharacter {
         for(int i = 0; i < inputArr.length; i++){
             if(!newInput.contains(String.valueOf(inputArr[i]))){
                 newInput += String.valueOf(inputArr[i]);
+                total ++;
+            }
+        }
+        return total;
+    }
+
+    static int totalCharacter3(String input){
+        char[] inputArr = input.toLowerCase().toCharArray();
+        int total = 0;
+        List<Character> list = new ArrayList<>();
+        for(int i = 0; i < inputArr.length; i++){
+            if(!list.contains(inputArr[i])){
+                list.add(inputArr[i]);
                 total ++;
             }
         }
