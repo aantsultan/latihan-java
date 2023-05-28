efec1ac37aece843f13b0d847c2e6d76c956bc5a
Your <aant.sultan@gmail.com>
2023-05-28 15:32:35 +0700

    init module latihan-java-spring-oauth2

---
+++ latihan-java-spring-oauth2/README.md
@@ -0,0 +1,3 @@
+Latihan Java Spring OAuth2
+--
+This module is used to exercise the basic authorization
---
+++ latihan-java-spring-oauth2/src/main/java/com/latihan/java/spring/oauth2/LatihanJavaSpringOauth2Main.java
@@ -0,0 +1,12 @@
+package com.latihan.java.spring.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanJavaSpringOauth2Main {
+
+    public static void main (String[] args){
+        SpringApplication.run(LatihanJavaSpringOauth2Main.class, args);
+    }
+}
