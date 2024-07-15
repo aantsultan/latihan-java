b9369f7337fa2b369220ab319df6bea7f1482545
A-Ant Sultan Rahmanya <a-ant.sultan@sakura-system.co.id>
2024-07-15 15:57:15 +0700

    # latihan-java-monitoring, initial commit

---
+++ latihan-java-monitoring/src/main/java/com/latihan/java/monitoring/LatihanJavaMonitoringMain.java
@@ -0,0 +1,13 @@
+package com.latihan.java.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatihanJavaMonitoringMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatihanJavaMonitoringMain.class, args);
+    }
+
+}
---
+++ latihan-java-monitoring/src/main/java/com/latihan/java/monitoring/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.latihan.java.monitoring.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public ResponseEntity<String> get(){
+        return ResponseEntity.ok("OK");
+    }
+
+}
